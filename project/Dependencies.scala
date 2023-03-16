396f8eb4066b1ee034d9b0e3f96c2b9dbee10744
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-03-16 08:34:54 +0000

    Update tdr-backend-checks-utils to 0.1.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object Dependencies {
   private val circeVersion = "0.14.5"
 
-  lazy val backendCheckUtils = "uk.gov.nationalarchives" %% "tdr-backend-checks-utils" % "0.1.6"
+  lazy val backendCheckUtils = "uk.gov.nationalarchives" %% "tdr-backend-checks-utils" % "0.1.7"
   lazy val circeCore = "io.circe" %% "circe-core" % circeVersion
   lazy val circeParser = "io.circe" %% "circe-parser" % circeVersion
   lazy val circeGeneric = "io.circe" %% "circe-generic" % circeVersion
